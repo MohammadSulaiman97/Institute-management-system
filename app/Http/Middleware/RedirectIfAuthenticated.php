9f64599e7d93c3438828bc3b382ab614214856a3
asus <mohammadsulaiman870@gmail.com>
2021-05-14 20:56:05 +0300

    first commit

---
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Providers\RouteServiceProvider;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class RedirectIfAuthenticated
+{
+
+    public function handle($request, Closure $next, $guard = null)
+    {
+
+        if (Auth::guard($guard)->check()) {
+            if($guard === 'admin') {
+                return redirect('/admin/home');
+            }
+            return redirect('/home');
+        }
+
+        return $next($request);
+    }
+
+}
